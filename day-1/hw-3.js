ef488d93d743c7d030e9328a2caa756fd6dc53dc
saba jokhadze <saba.joxadze.04@gmail.com>
2023-07-12 03:27:43 +0400

    commiting homeworks

---
+++ day-1/hw-3.js
@@ -0,0 +1,6 @@
+let randomNumber = Math.floor(Math.random() * 10);
+while (randomNumber != 5) {
+  randomNumber = Math.floor(Math.random() * 10);
+  console.log("not 5");
+  console.log(randomNumber);
+}
