f5123ba4fc39e5c682d2dbc5b3f7b9a2f5076326
saba jokhadze <saba.joxadze.04@gmail.com>
2023-08-05 16:51:24 +0400

    hws about classes

--- day-8/classes/hw-1.js
+++ day-8/classes/hw-1.js
@@ -9,7 +9,8 @@ class Validator {
     return /^(0?[1-9]|[12][0-9]|3[01])\.(0?[1-9]|1[0-2])\.\d{4}$/.test(date);
   }
   isPhone(number) {
-    return /^(?:\+995\s?)?(?:5\d{2}\s?\d{2}\s?\d{2}\s?\d{2})$/.test(number);
+    return /^(?:\+995\s?)?(?:5\d{2}\s?\d{2}\s?\d{2}\s?\d{2})$/.test(number); //georgian number +995 not necessary
   }
 }
 const validator = new Validator();
+console.log(validator.isPhone("555 54 20 34"));
